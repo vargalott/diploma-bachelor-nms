70ac5c7cf9e0a1e65c94ce5429d5ecc6a198c2b5
vargalott <156085832+vargalott@users.noreply.github.com>
2021-05-07 10:44:13 +0300

    update .gitignore

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Install packages as defined in this file into the Python environment."""
+from setuptools import setup, find_packages
+
+setup(
+    name="diploma_nm-s",
+    author="andinoriel",
+    author_email="simonnikolaj20@gmail.com",
+    url="https://github.com/andinoriel/diploma_nm-s",
+    description="simple web server for diploma module:network",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "setuptools>=45.0",
+        "falcon>=3.0.0",
+        "requests>=2.25.1",
+        "gunicorn>=20.1.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "diploma_nm-s=diploma_nm-s.__main__:main",
+        ]
+    },
+)
