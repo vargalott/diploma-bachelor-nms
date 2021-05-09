376758fa48590316639dd69c27c4d50ade50f6f7
vargalott <156085832+vargalott@users.noreply.github.com>
2021-05-09 10:13:26 +0300

    update server on_get behaviour

--- diploma-nms/__main__.py
+++ diploma-nms/__main__.py
@@ -1,10 +1,12 @@
 import wsgiref.simple_server
 import falcon
 
+import random
+import string
 
 class MainController:
     def on_get(self, req, resp):
-        resp.text = "Greetings, traveller"
+        resp.text = "".join(random.choice(string.digits) for i in range(1024))
         resp.status = falcon.HTTP_200
 
 
@@ -12,6 +14,6 @@ if __name__ == '__main__':
     app = application = falcon.App()
     app.add_route("/", MainController())
 
-    with wsgiref.simple_server.make_server('', 8000, app) as httpd:
-        print('Serving on port 8000...')
+    with wsgiref.simple_server.make_server("", 4723, app) as httpd:
+        print('Serving on port 4723...')
         httpd.serve_forever()
